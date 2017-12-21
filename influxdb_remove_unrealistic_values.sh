a8943fdc445f62f44a01785e989d4804698fd5f8
Andreas Hubert <andreas.hubert@censhare.com>
2017-12-21 09:28:03 +0100

    remove echo

--- influxdb_remove_unrealistic_values.sh
+++ influxdb_remove_unrealistic_values.sh
@@ -7,6 +7,6 @@ devices=$(influx -database "$db" -execute "show series" | grep "probe=$probe" | 
 for device in $devices; do
     times=$(influx -database "$db" -execute "select level from \"$device\" where level > $maxval;" -format "csv" | grep -v "name,time,level" | cut -f 2 -d ",");
     for time in $times; do
-        echo influx -database "$db" -execute "delete from \"$device\" where time = $time;";
+        influx -database "$db" -execute "delete from \"$device\" where time = $time;";
     done;
 done
