f4053080f97b539e7455ce13861677766dfb91d2
Andreas Hubert <andreas.hubert@censhare.com>
2017-12-21 09:16:53 +0100

    add script to remove unrealistic values from influxdb

---
+++ influxdb_remove_unrealistic_values.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+db=zway
+probe=temperature
+maxval=50
+
+devices=$(influx -database "$db" -execute "show series" | grep "probe=$probe" | cut -f 1 -d ",")
+for device in $devices; do
+    times=$(influx -database "$db" -execute "select level from \"$device\" where level > $maxval;" -format "csv" | grep -v "name,time,level" | cut -f 2 -d ",");
+    for time in $times; do
+        echo influx -database "$db" -execute "delete from \"$device\" where time = $time;";
+    done;
+done
